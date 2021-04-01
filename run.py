73ca81ed1994a0021aafa8a9422825f6df15fbdf
Steve Watson <stevewatsondev@gmail.com>
2021-04-01 11:50:31 +0100

    ON HEROKU

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 from market import app
+import os
+ON_HEROKU = os.environ.get('ON_HEROKU')
+
+if ON_HEROKU:
+    # get the heroku port
+    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+else:
+    port = 5000
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=port)
